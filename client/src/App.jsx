6254122e0dbab2b74967dfc14a47631595ad816e
Ghaby-X <gabrielanyaele21@gmail.com>
2024-01-04 20:25:26 +0000

    feat: completed Login UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [page, setPage] = useState('Login')
-  const [pageText, setPageText] = useState('sign into your account')
   const [email, setEmail] = useState('')
   const[password, setPassword] = useState('')
 
@@ -11,17 +9,18 @@ function App() {
     <>
     <div class="body h-screen w-screen flex justify-center items-center">
       <div className='form-body bg-slate-800 w-[450px] rounded-3xl pt-2 px-[40px]'>
-        <h4 className="text-slate-200 text-[50px] text-center font-bold">{page}</h4>
-        <p className='text-slate-200 text-center text-[20px] mb-[60px]'>{pageText}</p>
+        <h4 className="text-slate-200 text-[50px] text-center font-bold">Login</h4>
+        <p className='text-slate-200 text-center text-[20px] mb-[60px]'>sign into your account</p>
 
         <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your email</p>
-        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-14 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail('e')}}></input>
+        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail('e')}}></input>
         
         <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your password</p>
-        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-14 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword('e')}}></input>
+        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword('e')}}></input>
 
-
-        <button></button>
+        <button className='text-white bg-green-700 h-12 w-[100%] rounded-xl mb-[30px]'>Sign-In</button>
+        
+        <p className='text-white text-center'>New here? Register</p>
       </div>
     </div>
       
