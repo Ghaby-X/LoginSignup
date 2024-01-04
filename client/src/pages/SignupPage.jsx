eaf0faca7b7a3fc23621ae14e48dbd6f8992417d
Ghaby-X <gabrielanyaele21@gmail.com>
2024-01-04 20:45:35 +0000

    feat: added Signup page

---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function SignupPage() {
+    const [email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+  
+    return (
+      <>
+      <div class="body h-screen w-screen flex justify-center items-center">
+        <div className='form-body bg-slate-800 w-[450px] rounded-3xl pt-2 px-[40px]'>
+          <h4 className="text-slate-200 text-[50px] text-center font-bold">SignUp</h4>
+          <p className='text-slate-200 text-center text-[20px] mb-[60px]'>create an account</p>
+  
+          <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your email</p>
+          <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail('e')}}></input>
+          
+          <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your password</p>
+          <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword('e')}}></input>
+  
+          <button className='text-white bg-green-700 h-12 w-[100%] rounded-xl mb-[30px]'>Register</button>
+          
+          <p className='text-white text-center mb-3'>Already a user? Login</p>
+        </div>
+      </div>
+        
+      </>
+    )
+  }
+  
+  export default SignupPage
+  
