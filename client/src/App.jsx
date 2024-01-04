c06feaa8d7a861f6640f41fe1265fa685320c2f4
Ghaby-X <gabrielanyaele21@gmail.com>
2024-01-04 20:00:22 +0000

    feat: added email and password ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,23 @@ function App() {
   const [page, setPage] = useState('Login')
   const [pageText, setPageText] = useState('sign into your account')
   const [email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
 
   return (
     <>
     <div class="body h-screen w-screen flex justify-center items-center">
-      <div className='form-body bg-slate-800 w-[500px] rounded-3xl pt-2 px-[40px]'>
-        <h4 className="text-slate-200 text-[30px] text-center font-bold">{page}</h4>
-        <p className='text-slate-200 text-center text-[15px] mb-[60px]'>{pageText}</p>
-        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-14 mb-12" onChange={e => {setEmail('e')}}></input>
-        
+      <div className='form-body bg-slate-800 w-[450px] rounded-3xl pt-2 px-[40px]'>
+        <h4 className="text-slate-200 text-[50px] text-center font-bold">{page}</h4>
+        <p className='text-slate-200 text-center text-[20px] mb-[60px]'>{pageText}</p>
 
+        <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your email</p>
+        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-14 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail('e')}}></input>
+        
+        <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your password</p>
+        <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-14 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword('e')}}></input>
+
+
+        <button></button>
       </div>
     </div>
       
