55578bc9d2ded723f4869fd4edd43f21c22ec8b7
Ghaby-X <gabrielanyaele21@gmail.com>
2024-01-05 06:32:09 +0000

    feat: add password validation to handleSubmits

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,26 +3,70 @@ import { Link } from 'react-router-dom'
 
 function LoginPage() {
     const [email, setEmail] = useState('')
-    const[password, setPassword] = useState('')
-  
+    const [password, setPassword] = useState('')
+    const [ error, setError] = useState()
+    const [ response, setResponse] = useState()
+
+    const loginEndpoint = ''
+    const passwordRegex = /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$"/;
+
+    async function handleLogin(e) {
+        e.preventDefault()
+        setError(null)
+
+        if (passwordRegex.test(password) == false) {
+            setError({msg: 'Password must be between 8-16 characters \n must contain atleast, one letter, one number, one uppercase'})
+            console.log(error)
+            return
+        }
+        /*
+        try {
+            const res = await fetch(loginEndpoint, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({
+                    email: email,
+                    password: password
+                })
+            })
+
+            setResponse(response.json())
+            console.log(response)
+
+        }
+        catch(err){
+            console.log(err.message)
+            setError({msg: err.message})
+            
+        }
+        finally{
+            console.log("just finally")
+        }
+        */
+    }
+
+    
     return (
       <>
-      <div class="body h-screen w-screen flex justify-center items-center">
-        <div className='form-body bg-slate-800 w-[450px] rounded-3xl pt-2 px-[40px]'>
-          <h4 className="text-slate-200 text-[50px] text-center font-bold">Login</h4>
-          <p className='text-slate-200 text-center text-[20px] mb-[60px]'>sign into your account</p>
-  
-          <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your email</p>
-          <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail('e')}}></input>
+      <form className="body h-screen w-screen flex justify-center items-center" onSubmit={handleLogin}>
+        <div className='form-body bg-slate-800 w-[450px] rounded-xl py-8 px-[40px] shadow-2xl'>
+          <h4 className="text-slate-200 text-4xl text-center tracking-tight font-bold ">Login</h4>
+          <p className='text-center text-lg text-gray-400 mb-10'>sign in to your account</p>
+
+            {error?.msg && <p className='text-red-400 text-sm text-center mb-2'>{error.msg}</p>}
+          <p className='text-slate-200 text-md font-semibold mb-[10px]'>Enter your email</p>
+          <input required type="email" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail(e.target.value)}}></input>
           
-          <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your password</p>
-          <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword('e')}}></input>
+          <p className='text-slate-200 text-md font-semibold mb-[10px]'>Enter your password</p>
+          <input required type="password" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword(e.target.value)}}></input>
   
-          <button className='text-white bg-green-700 h-12 w-[100%] rounded-xl mb-[30px]'>Sign-In</button>
+          <button type='' className='text-white bg-green-700 h-12 w-[100%] rounded-xl mb-[30px] hover:bg-green-600'>Sign-In</button>
           
-          <p className='text-white text-center mb-3'>New here? <Link className='text-blue-400' to='/signup'>Register</Link></p>
+          <p className='text-white text-center text-sm mb-3'>New here? <Link className='text-blue-400' to='/signup'>Register</Link></p>
         </div>
-      </div>
+      </form>
         
       </>
     )
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -3,26 +3,41 @@ import { Link } from 'react-router-dom'
 
 function SignupPage() {
     const [email, setEmail] = useState('')
-    const[password, setPassword] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState()
+
+    const passwordRegex = /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$"/;
+
+
+    async function handleRegister(e) {
+      e.preventDefault()
+      setError()
+
+      if (passwordRegex.test(password) == false) {
+          setError({msg: 'Password must be between 8-16 characters \n must contain atleast, one letter, one number, one uppercase'})
+          return
+      }
+    }
   
     return (
       <>
-      <div class="body h-screen w-screen flex justify-center items-center">
-        <div className='form-body bg-slate-800 w-[450px] rounded-3xl pt-2 px-[40px]'>
-          <h4 className="text-slate-200 text-[50px] text-center font-bold">SignUp</h4>
-          <p className='text-slate-200 text-center text-[20px] mb-[60px]'>create an account</p>
-  
-          <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your email</p>
-          <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail('e')}}></input>
+      <form className="body h-screen w-screen flex justify-center items-center" onSubmit={handleRegister}>
+        <div className='form-body bg-slate-800 w-[450px] rounded-xl py-8 px-[40px] shadow-2xl'>
+          <h4 className="text-slate-200 text-4xl text-center tracking-tight font-bold">Sign Up</h4>
+          <p className='text-center text-lg text-gray-400 mb-10'>create an account</p>
+
+          {error?.msg && <p className='text-red-400 text-sm text-center mb-2'>{error.msg}</p>}
+          <p className='text-slate-200 text-md  font-semibold mb-[10px]'>Enter your email</p>
+          <input required type="email" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="email" onChange={e => {setEmail(e.target.value)}}></input>
           
-          <p className='text-slate-200 text-[20px] mb-[10px]'>Enter your password</p>
-          <input type="text" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword('e')}}></input>
+          <p className='text-slate-200 text-md font-semibold mb-[10px]'>Enter your password</p>
+          <input required type="password" className="w-[100%] bg-slate-600 rounded-xl min-h-12 mb-8 px-[12px] text-white" placeholder="password" onChange={e => {setPassword(e.target.value)}}></input>
   
-          <button className='text-white bg-green-700 h-12 w-[100%] rounded-xl mb-[30px]'>Register</button>
+          <button type='submit' className='text-white bg-green-700 h-12 w-[100%] rounded-xl mb-[30px] hover:bg-green-600'>Register</button>
           
-          <p className='text-white text-center mb-3'>Already a user? <Link className='text-blue-400' to='/'>Login</Link></p>
+          <p className='text-white text-center text-sm mb-3'>Already a user? <Link className='text-blue-400' to='/'>Login</Link></p>
         </div>
-      </div>
+      </form>
         
       </>
     )
