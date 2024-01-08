7ac48a432a1632f9ebbe87e3984566fc75dbb7f9
Ghaby-X <gabrielanyaele21@gmail.com>
2024-01-08 13:39:49 +0000

    refactor: added routes to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 
+const userRoutes = require('./routes/route')
+
 const PORT = process.env.PORT || 4000
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(cors)
 
+app.use('/user', userRoutes)
 
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
