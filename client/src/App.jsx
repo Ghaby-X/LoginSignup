c2054f8cb2cae2f812242c6ad2c4a4cef681acc9
Ghaby-X <gabrielanyaele21@gmail.com>
2024-02-02 23:44:31 +0000

    configure routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Home from './pages/Home'
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
 import DashboardPage from "./pages/DashboardPage"
 import MailSent from "./pages/MailSent"
 import MailVerified from "./pages/MailVerified"
+import RequireAuth from "./wrapper/RequireAuth"
 
 function App() {
   return(
     <Routes>
-      <Route path='/' element={<LoginPage />} />
+      <Route path = '/' element= {<Home />} />
+      <Route path='/login' element={<LoginPage />} />
       <Route path='/signup' element = {<SignupPage/>} />
       <Route path='/mailsent' element = {<MailSent/>} />
       <Route path='/mailverification/:id/:uniqueString' element = {<MailVerified/>} />
-      <Route path='/dashboard' element = {<DashboardPage/>} />
+      <Route path='/dashboard' element = {
+        <RequireAuth>
+          <DashboardPage/>
+        </RequireAuth>
+        
+      } />
     </Routes>
   )
 }
